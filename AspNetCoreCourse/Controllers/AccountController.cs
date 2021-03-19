47c5044618e02d3a4bd4fc59587eae8cb834aa7e
Anastasia Sidorchuk <vedroid28@mail.ru>
2021-03-19 11:01:03 +0500

    Добавьте файлы проекта.

---
+++ AspNetCoreCourse/Controllers/AccountController.cs
@@ -0,0 +1,76 @@
+﻿using AspNetCoreCourse.Models;
+using AspNetCoreCourse.ViewModels;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace AspNetCoreCourse.Controllers
+{
+    public class AccountController : Controller
+    {
+        static Dictionary<string, string> users = new Dictionary<string, string>
+        {
+            {"student","123" },
+            {"Kate","dino121" },
+            {"UFO28","helloWorld" }
+        };
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Anonim()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = new User(model.Login, model.Password);
+                if (users.ContainsKey(user.Login))
+                {
+                    if (users[user.Login]==user.Password)
+                    {
+                        await Authenticate(model.Login);
+                        return RedirectToAction("Index", "Home");
+                    }
+                    ModelState.AddModelError("", "Неверный пароль");
+                }
+                else{ModelState.AddModelError("", "Пользователя не существует");}
+            }
+            return View(model);
+        }
+        private async Task Authenticate(string userName)
+        {
+            // создаем один claim
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimsIdentity.DefaultNameClaimType, userName),
+                new Claim("authentification_datetime", DateTime.Now.ToString())
+            };
+            // создаем объект ClaimsIdentity
+            ClaimsIdentity id = new ClaimsIdentity(claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
+            // установка аутентификационных куки
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(id));
+        }
+
+        public async Task<IActionResult> Signout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Login", "Account");
+        }
+    }
+}
---
+++ AspNetCoreCourse/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreCourse.Models
+{
+    public class User
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+        public User(String Login, String Password)
+        {
+            this.Login = Login;
+            this.Password = Password;
+        }
+    }
+}
